72082bd223c72627af4cef2c95eddaf64c81786c
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2024-01-07 19:56:16 -0800

    More stuffs

--- GA_GitHub_Basics_JonathanReed/Program.cs
+++ GA_GitHub_Basics_JonathanReed/Program.cs
@@ -3,5 +3,6 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, Professor!");
+        Console.WriteLine("It's Jonathan, I'd like to get good enough at programming so that companies can't ignore the value I bring and pay me a fair wage");
     }
 }
